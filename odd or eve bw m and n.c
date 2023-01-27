ab3f3cc4a125d4ee8050c4c34065b38893924e75
pavithra192224034 <123437608+pavithra192224034@users.noreply.github.com>
2023-01-27 21:24:32 +0530

    Add files via upload

---
+++ odd or eve bw m and n.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main() {
+ int num,m,n,i;
+ printf("Enter start range :");
+ scanf("%d",&n);
+ printf("Enter end range :");
+ scanf("%d",&m);
+if(m>0 && n>0 && m>n){
+	printf("odd:\n");
+ for(i=n;i<m;i++){
+ 	if(i%2!=0){
+ 		printf("%d\n",i);
+	 }
+ }
+ printf("even:\n");
+ for(i=n;i<m;i++){
+ 	if(i%2==0){
+ 		printf("%d\n",i);
+	 }
+ }
+}
+else{
+	printf("invalid input");
+}
+}
+
